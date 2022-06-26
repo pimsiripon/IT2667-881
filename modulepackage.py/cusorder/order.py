db747e906866a013054f9bbac2ece8055a364534
pimsiripon <644708020005@dpu.ac.th>
2022-06-26 15:02:24 +0700

    packageorder

---
+++ modulepackage.py/cusorder/order.py
@@ -0,0 +1,11 @@
+class Order:
+    def __init__(self,date,status) -> None:
+        self.date = date
+        self.status = status
+
+    def calc_tex(self):
+        pass
+    def cal_total(self):
+        pass
+    def cal_total_weight(self):
+        pass
---
+++ modulepackage.py/practice_package.py
@@ -0,0 +1,7 @@
+from cusorder import customer,order
+
+cus = customer.Customer("Jame","Nontaburi")
+ord = order.Order("15-06-2022","packed")
+
+print(f'Order of {cus.cus_name} on {ord.date} is {ord.status}')
+
